13e05aa447ab751d2a8cb66b8f9fa1451b9be203
Plasmabot1 <37595571+Plasmabot1@users.noreply.github.com>
2019-01-18 23:26:00 -0500

    Add files via upload

---
+++ Calculator.ino
@@ -0,0 +1,204 @@
+#include <QueueArray.h>
+#include <USBHID.h>
+#include "SPI.h"
+#include <Adafruit_GFX_AS.h>    // Core graphics library, with extra fonts.
+#include <Adafruit_ILI9341_STM.h> // STM32 DMA Hardware-specific library
+#include <Wire.h>
+//#include <Fonts/FreeMonoBold12pt7b.h>
+
+#define TFT_CS         PA4                  
+#define TFT_DC         PA3                
+#define TFT_RST        PA2 
+
+#define MODE           PB11
+#define BATTERY        PA1
+#define NUMLK          PB10
+#define SDA            PB7
+#define SCL            PB6
+#define INT            PB5
+
+#define ADDR0          32
+#define ADDR1          33
+
+
+Adafruit_ILI9341_STM tft = Adafruit_ILI9341_STM(TFT_CS, TFT_DC, TFT_RST); // Use hardware SPI
+
+bool mode = 1; //1 = numpad, 0 = calc
+bool numlk = 0;
+const uint8_t lookupTable[2][22] = {{KEY_F1, KEY_F2, KEY_F3, 0, 0, '/', '*', KEY_BACKSPACE, '7', '8', '9', '-', '4', '5', '6', '+', '1', '2', '3', KEY_RETURN, '0', '.'},
+                                    {KEY_F1, KEY_F2, KEY_F3, 0, 0, '/', '*', KEY_BACKSPACE, KEY_HOME, KEY_UP_ARROW, KEY_PAGE_UP, '-', KEY_LEFT_ARROW, '5', KEY_RIGHT_ARROW, '+', KEY_END, KEY_DOWN_ARROW, KEY_PAGE_DOWN, KEY_RETURN, KEY_INSERT, KEY_DELETE}};
+QueueArray <uint8_t> keyQueue;
+
+String labels[4] = {"F1", "F2", "F3", "LIGHTING"};
+
+String expressions[6];
+
+void setup() {
+  tft.begin();
+  tft.setRotation(1);
+  tft.fillScreen(ILI9341_BLACK);
+  //tft.setFont(&FreeMonoBold12pt7b);
+  tft.setTextWrap(false);
+  pinMode(INT, INPUT);
+  attachInterrupt(INT, ISR, FALLING);
+  pinMode(MODE, INPUT);
+  //mode = digitalRead(MODE);
+  pinMode(BATTERY, INPUT);
+  Wire.begin();
+  
+  Wire.beginTransmission(ADDR0);
+  Wire.write(0b00000110);
+  Wire.write(0b11111111);
+  Wire.write(0b11111111);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(ADDR1);
+  Wire.write(0b00000110);
+  Wire.write(0b11111111);
+  Wire.write(0b11111111);
+  Wire.endTransmission();
+
+  if(mode){
+    USBHID_begin_with_serial(HID_BOOT_KEYBOARD);
+    BootKeyboard.begin();
+    delay(1000);
+    functionTabs();
+    //tft.drawFastHLine
+  } else {
+    labels[0] = "SYMBOLS";
+    labels[1] = "TRIGONOMETRY";
+    labels[2] = "VARIABLES";
+    labels[3] = "MENU";
+    
+  }
+}
+
+void ISR(){
+  byte data[3] = {0b11111111, 0b11111111, 0b11111111};
+  Wire.beginTransmission(ADDR0);
+  Wire.write(0b00000000);
+  Wire.endTransmission();
+  Wire.requestFrom(ADDR0, 2);
+  if(Wire.available()) {
+    data[0] =  Wire.read();
+  }
+  if(Wire.available()) {
+    data[1] = Wire.read();
+  }
+    
+  Wire.beginTransmission(ADDR1);
+  Wire.write(0b00000000);
+  Wire.endTransmission();
+  Wire.requestFrom(ADDR1, 1);
+  if(Wire.available()) {
+    data[2] = Wire.read();
+  }
+    
+  uint8_t x = 0;
+  for(int i = 0; i < 3; i++){
+    while(data[i] & 0b00000001 != 0){
+      data[i] = data[i] >> 1;
+      x++;
+    }
+    if(x != 8*(i+1)){
+      break;
+    }
+  }
+  if(x != 24){
+    keyQueue.push(x);
+  }
+}
+
+void functionTabs(){
+  tft.setTextColor(ILI9341_BLACK);
+  tft.setTextSize(1);
+  tft.fillRect(0, 228, 320, 12, tft.color565(150, 150, 150));
+  tft.fillTriangle(75, 228, 80, 240, 85, 228, ILI9341_BLACK);
+  tft.fillTriangle(155, 228, 160, 240, 165, 228, ILI9341_BLACK);
+  tft.fillTriangle(235, 228, 240, 240, 245, 228, ILI9341_BLACK);
+  for(int x = 0; x < 4; x++){
+    tft.setCursor(x*80+40-(int)(strlen(labels[x].c_str())*2.5), 230);
+    tft.print(labels[x]);
+  }
+}
+
+void header(){
+  float battery = analogRead(BATTERY);
+  uint16_t color;
+  tft.setTextSize(1);
+  tft.fillRect(0, 0, 320, 12, tft.color565(150, 150, 150));
+  tft.drawRect(2, 2, 16, 8, ILI9341_WHITE);
+  tft.fillRect(18, 4, 2, 4, ILI9341_WHITE);
+  tft.setCursor(28, 2);
+  if(battery >= 2900){
+    if(battery >= 3351){
+      color = tft.color565((int)(255*(3910-battery)/1118), 255, 0);
+    } else {
+      color = tft.color565(255, (int)(255*(3910-battery)/1118), 0);
+    }
+    tft.fillRect(3, 3, (int)((battery-2792)/1118*14), 6, color);
+    tft.setTextColor(color);
+    tft.print((battery-2792)/1118*100);
+    tft.print("% ");
+  } else {
+    tft.fillRect(3, 3, (int)((battery-2792)/1118*14), 6, ILI9341_RED);
+    tft.drawRect(2, 2, 16, 8, ILI9341_RED);
+    tft.fillRect(18, 4, 2, 4, ILI9341_RED);
+    tft.setTextColor(ILI9341_RED);
+    tft.print((battery-2792)/1118*100);
+    tft.print("% LOW BATTERY! ");
+  }
+  tft.setTextColor(ILI9341_BLACK);
+
+  if(mode){
+    tft.print("MODE: NUMPAD ");
+    if(numlk){
+      tft.print("NUMLOCK OFF");
+    } else {
+      tft.print("NUMLOCK ON");
+    }
+  } else {
+    tft.print("MODE: CALCULATOR ");
+  }
+}
+
+void symbolMenu(){
+  tft.fillRect(20, 20, 280, 200, tft.color565(150, 150, 150));
+}
+
+unsigned long t = millis();
+
+void loop(){
+  if(mode){
+    while(1){
+      if(!keyQueue.isEmpty()){
+        if(keyQueue.peek() == 3){
+          digitalWrite(NUMLK, 1);
+          delay(10);
+          digitalWrite(NUMLK, 0);  
+          keyQueue.pop();      
+        } else if(keyQueue.peek() == 4){
+          numlk = 1-numlk;
+          header();
+          keyQueue.pop();
+        } else {
+          BootKeyboard.press(lookupTable[numlk][keyQueue.peek()]);
+          delay(10);
+          BootKeyboard.release(lookupTable[numlk][keyQueue.pop()]);
+          delay(10);
+        }
+      }
+      if(millis() - t >= 500){
+        header();
+        t = millis();
+      }
+    }
+  } else {
+    while(1){
+      header();
+      functionTabs();
+      
+    }
+  }
+}
+
---
+++ CalculatorLEDs.ino
@@ -0,0 +1,145 @@
+/* This example shows how to display a moving rainbow pattern on
+ * an APA102-based LED strip. */
+
+/* By default, the APA102 library uses pinMode and digitalWrite
+ * to write to the LEDs, which works on all Arduino-compatible
+ * boards but might be slow.  If you have a board supported by
+ * the FastGPIO library and want faster LED updates, then install
+ * the FastGPIO library and uncomment the next two lines: */
+// #include <FastGPIO.h>
+// #define APA102_USE_FAST_GPIO
+
+#include <APA102.h>
+
+//#define XCOORD (shapes[x][1] >> 4)
+//#define YCOORD (shapes[x][1] && 0b00001111)
+
+//uint8_t points[32] = {0, 6, 10, 12, 18, 22, 16, 14, 10, 4, 2, 6, 8, 14, 18, 20, 3, 5, 9, 15, 17, 21, 19, 13, 9, 7, 1, 5, 11, 13, 17, 23};
+
+// Define which pins to use.
+const uint8_t dataPin = 3;
+const uint8_t clockPin = 4;
+volatile int mode = 0;
+volatile int x = 0;
+
+// Create an object for writing to the LED strip.
+APA102<dataPin, clockPin> ledStrip;
+
+// Create a buffer for holding the colors (3 bytes per color).
+volatile rgb_color colors[24];
+
+// Set the brightness to use (the maximum is 31).
+const uint8_t brightness = 1;
+
+void setup()
+{
+  GIMSK = 0b00100000;    // turns on pin change interrupts
+  PCMSK = 0b00000001;    // turn on interrupts on pins PB0
+  sei();                 // enables interrupts
+  pinMode(1, INPUT);
+  randomSeed(analogRead(1));
+  mode = 0;
+  //for(uint8_t i = 0; i < 24; i++){
+  //  colors[i] = rgb_color(0, 0, 0);
+  //}
+  //ledStrip.write(colors, 24, brightness);
+  //delay(1000);
+}
+
+/* Converts a color from HSV to RGB.
+ * h is hue, as a number between 0 and 360.
+ * s is the saturation, as a number between 0 and 255.
+ * v is the value, as a number between 0 and 255. */
+rgb_color hsvToRgb(uint16_t h, uint8_t s, uint8_t v)
+{
+    //h=h%360;
+    uint8_t f = (h % 60) * 255 / 60;
+    uint8_t p = (255 - s) * (uint16_t)v / 255;
+    uint8_t q = (255 - f * (uint16_t)s / 255) * (uint16_t)v / 255;
+    uint8_t t = (255 - (255 - f) * (uint16_t)s / 255) * (uint16_t)v / 255;
+    uint8_t r = 0, g = 0, b = 0;
+    switch((h / 60) % 6){
+        case 0: r = v; g = t; b = p; break;
+        case 1: r = q; g = v; b = p; break;
+        case 2: r = p; g = v; b = t; break;
+        case 3: r = p; g = q; b = v; break;
+        case 4: r = t; g = p; b = v; break;
+        case 5: r = v; g = p; b = q; break;
+    }
+    return rgb_color(r, g, b);
+}
+
+ISR(PCINT0_vect){
+  mode++;
+  if(mode == 4){
+    mode = 0;
+  }
+  x = 0;
+  for(uint8_t i = 0; i < 24; i++){
+    colors[i] = rgb_color(0, 0, 0);
+  }
+}
+
+int offset = 0;
+int y = 0;
+void loop(){
+  switch(mode){
+    case 0:
+      for(int i = 0; i < 24; i++){
+        if(y){
+          if(i <= x){
+            colors[23-i] = hsvToRgb(offset+4*(i+1), 255, 255);
+          } else {
+            colors[23-i] = hsvToRgb(offset+4*(25-i)+300, 255, 255);
+          }
+        } else {
+          if(i <= x){
+            colors[i] = hsvToRgb(offset+4*(i+1), 255, 255);
+          } else {
+            colors[i] = hsvToRgb(offset+4*(25-i)+300, 255, 255);
+          }
+        }
+      }
+      ledStrip.write(colors, 24, brightness);
+      x++;
+      delay(50);
+      if(x == 24){
+        x = 0;
+        y = 1-y;
+        offset = offset + 60;
+        offset = offset % 360;
+      }
+      break;
+      
+    case 1:
+      for(uint8_t i = 0; i < 24; i++){
+        colors[i] = hsvToRgb((x + i * 8)%360, 255, 255);
+      }
+      x = x%360;
+      x = x+8;
+      ledStrip.write(colors, 24, brightness);
+      delay(10);
+      break;
+
+    case 2:
+      for(uint8_t i = 0; i < 24; i++){
+        colors[i] = rgb_color(0, 0, 0);
+      }
+      colors[(x+23)%24] = hsvToRgb(15*(x-1)+offset, 50, 50);
+      colors[x] = hsvToRgb(15*x+offset, 255, 255);
+      colors[(x+1)%24] = hsvToRgb(15*(x-1)+offset, 50, 50);
+      
+      colors[(x+11)%24] = hsvToRgb(15*(x-1)+offset+180, 50, 50);
+      colors[(x+12)%24] = hsvToRgb(15*x+offset+180, 255, 255);
+      colors[(x+13)%24] = hsvToRgb(15*(x-1)+offset+180, 50, 50);
+
+      ledStrip.write(colors, 24, brightness);
+      delay(50);
+      x++;
+      if(x==24){
+        x = 0;
+        offset = random(0, 360);
+      }
+      break;
+  }
+}
